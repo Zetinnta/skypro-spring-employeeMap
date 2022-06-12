59ea105bb77c3592f76634a8943f0c3f49e4d32e
Ян <Shatoshi@mail.ru>
2022-06-12 13:10:41 +0300

    Initial commit

---
+++ src/main/java/pro/sky/skyprospringemployeeMap/SkyproSpringEmployeeMapApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.skyprospringemployeeMap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyproSpringEmployeeMapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkyproSpringEmployeeMapApplication.class, args);
+	}
+
+}
